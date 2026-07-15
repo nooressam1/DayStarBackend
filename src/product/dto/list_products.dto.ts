09e53afe5ffafbef4cca716e6a5cb1f427ce85f2
Noor <nooressamjx2005@gmail.com>
2026-07-15 19:31:19 +0300

    Initial commit of DayStar Backend

---
+++ src/product/dto/list_products.dto.ts
@@ -0,0 +1,33 @@
+import { IsOptional, IsInt, Min, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class ListProductsDto {
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  limit?: number;
+
+  @IsOptional()
+  @IsString()
+  categoryId?: string;
+
+  @IsOptional()
+  @IsString()
+  collection?: string;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  discount?: number;
+}
